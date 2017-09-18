873a00647b7aeca982b62cee2b262ec720ff2963
ikranz <ianmkranz@gmail.com>
2017-09-18 18:57:59 -0400

    Add changes to IR Reader

--- InfraredRead/InfraredRead.ino
+++ InfraredRead/InfraredRead.ino
@@ -1,6 +1,5 @@
 // do #defines BEFORE #includes
-#define LOG_OUT 1 // use the log output function
-#define LIN_OUT8 1 // use the lin output function
+#define LOG_OUT 1 // use the log output functio
 #define FFT_N 256 // set to 256 point fft
 
 #include <FFT.h> // include the library
@@ -22,8 +21,6 @@ void setup()
   // sampling rate is [ADC clock] / [prescaler] / [conversion clock cycles]
   // for Arduino Uno ADC clock is 16 MHz and a conversion takes 13 clock cycles
   ADCSRA |= (1 << ADPS2) | (1 << ADPS0);    // 32 prescaler for 38.5 KHz
-  //ADCSRA |= (1 << ADPS2);                     // 16 prescaler for 76.9 KHz
-  //ADCSRA |= (1 << ADPS1) | (1 << ADPS0);    // 8 prescaler for 153.8 KHz
 
   ADCSRA |= (1 << ADATE); // enable auto trigger
   ADCSRA |= (1 << ADIE);  // enable interrupts when measurement complete
@@ -36,13 +33,15 @@ ISR(ADC_vect)
   if (i<512) {
     byte m = ADCL; // fetch adc data
     byte j = ADCH;
-    int k = (j << 8) | m; // form into an int
-    k -= 0x0200; // form into a signed int
-    k <<= 6; // form into a 16b signed int
+
+    // form into a 16b signed int
+    int k = (j << 8) | m;
+    k -= 0x0200;
+    k <<= 6;
+    
     fft_input[i] = k; // put real data into even bins
     fft_input[i+1] = 0; // set odd bins to 0
-    //Serial.println(k);
-    i++;
+    i=i+2;
   }
 }
   
@@ -54,16 +53,19 @@ void loop()
     fft_window(); // window the data for better frequency response
     fft_reorder(); // reorder the data before doing the fft
     fft_run(); // process the data in the fft
-    fft_mag_lin8(); // take the output of the fft
-    
+    fft_mag_log();
+    //fft_mag_lin8(); // take the output of the fft
+
     /*for (int k = 0; k < 128; k++)
-      Serial.println(fft_lin_out8[k]); // send out the data
+      Serial.println(fft_log_out[k]); // print FFT data
     while(1){}*/
+    
+    delay(1000);
     // restart
     i = 0;
-    for(int k = 25; k < 28; k++)
-      if(fft_lin_out8[k] > 8)Serial.println("7kHz Treasure");
-    for(int k = 21; k < 24; k++)
-      if(fft_lin_out8[k] > 6)Serial.println("12kHz Treasure");
+    for(int k = 47; k < 50; k++)
+      if(fft_log_out[k] > 170)Serial.println("7kHz Treasure");
+    for(int k = 81; k < 84; k++)
+      if(fft_log_out[k] > 170)Serial.println("12kHz Treasure");
   }
 }
