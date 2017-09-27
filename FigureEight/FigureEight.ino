383b78b4e761c646e3b07d61ebb63ff5e0696f3a
Felipe Fortuna <fb262@cornell.edu>
2017-09-27 14:11:00 -0400

    Helper functions

--- FigureEight/FigureEight.ino
+++ FigureEight/FigureEight.ino
@@ -6,69 +6,35 @@ int lineMidLeftPin, lineMidRightPin, lineRightPin, lineLeftPin; // Analog pins w
 int toleranceForward = 200; //
 int blackDetect = 600;      // Threshold above which sensors are reading a black line
 int turn = 0;               // Determines stage of 8-figure turn, values 0-7
-boolean startFlag = false;  // Set by pressing button, starts operation of robot.
 
 void setup() {
-  // Setup Serial and pin directions
-  Serial.begin(9600);
-  pinMode(startPin, INPUT);
-  // Initialize system variables
-  //Setup servos, pin 5 left wheel, pin 3 right wheel
-  leftWheel.attach(5);
-  rightWheel.attach(3);
-  lineMidLeftPin = A1;
-  lineMidRightPin = A2;
-  lineRightPin = A3;
-  lineLeftPin = A0;
+  initializeStuff();
+  waitForStart();
 }
 
 void loop() {
-  if (!startFlag){
-    if (digitalRead(startPin) == HIGH) startFlag = true; 
-    leftWheel.write(92);  //Stop left wheel moving
-    rightWheel.write(90); //Stop right wheel forward
-  }
-  
-  //Update line sensor values
-  lineMidLeft = analogRead(lineMidLeftPin); lineMidRight = analogRead(lineMidRightPin);
-  lineLeft = analogRead(lineLeftPin); lineRight = analogRead(lineRightPin);
+  // Update line sensor values
+  updateLineSensors();
   printSensors();
 
-  // Robot has begun
-  if (startFlag){
-      if (turn == 8) turn = 0; //Reset turn to 0 if it finishes 8 turns of figure eight
-
-      // Detect a crossroads
-      if (lineLeft > blackDetect && lineRight > blackDetect) { //left and right sensors
-        Serial.println("TURN!");
-        if (turn == 0 || turn == 4) keepStraight();
-        else if (turn > 0 && turn < 4) rightTurn();
-        else leftTurn();
-        turn += 1;   
-        Serial.println("TurnEnd");
-      }
-      
-      // Detects that it's on the black line
-      else if ((abs(lineMidLeft - lineMidRight) < toleranceForward))walkForward();
-
-      // If not on the black line, readjust
-      else if (lineMidLeft >= lineMidRight) leftDrift();      
-      else if (lineMidLeft < lineMidRight)  rightDrift();
+  //Reset turn to 0 if it finishes 8 turns of figure eight
+  if (turn == 8) turn = 0; 
+  
+  // Detect a crossroads
+  if (lineLeft > blackDetect && lineRight > blackDetect) { 
+    if (turn == 0 || turn == 4) keepStraight();
+    else if (turn > 0 && turn < 4) rightTurn();
+    else leftTurn();
+    turn += 1;   
   }
+  
+  // Detects that it's on the black line
+  else if ((abs(lineMidLeft - lineMidRight) < toleranceForward)) walkForward();
+  
+  // If not on the black line, readjust
+  else if (lineMidLeft >= lineMidRight) leftDrift();      
+  else if (lineMidLeft < lineMidRight)  rightDrift();
+
 }
 
-void printSensors(){
-    // Used for debugging
-    // Prints line sensor values left to right
-  Serial.print(lineLeft);
-  Serial.print("  ");
-  Serial.print(lineMidLeft);
-  Serial.print("  ");
-  Serial.print(lineMidRight);
-  Serial.print("  ");
-  Serial.print(lineRight);
-  Serial.print("  ");
-  Serial.print(turn);
-  Serial.print("  ");
-  Serial.println();
-}
+
--- FigureEight/MoveFunctions.ino
+++ FigureEight/MoveFunctions.ino
@@ -1,16 +1,24 @@
+//------------------------------------------------------------------------------
+// Turn Functions
+//------------------------------------------------------------------------------
+
 // Complete 90 degree left turn at intersection
 void leftTurn(){
   bool turnDone = false;
-  //walkForward();
-  //delay(200);
   bool flag = false;
+  walkForward();
+  delay(200);
+  
   while (! turnDone) {
-    leftTurnSlow(); //Begin turning left
+    // Begin turning left
+    leftTurnFast(); 
     delay(100);     
-    //Update needed sensor values
-    lineMidLeft = analogRead(lineMidLeftPin);
+
+    // Update needed sensor values
+    updateLineSensors();
+    printSensors();
     
-    //Test necessary conditions for completion of left turn
+    // Test if turn is complete
     if (!flag && lineMidLeft < blackDetect) flag = true;
     if (flag && lineMidLeft  > blackDetect) turnDone = true;
   }
@@ -19,61 +27,127 @@ void leftTurn(){
 // Complete 90 degree right turn at intersection
 void rightTurn(){
   bool turnDone = false;
-  //walkForward();
-  //delay(200);
   bool flag = false;
+  walkForward();
+  delay(200);
+  
   while (! turnDone) {
-    rightTurnSlow(); ///Begin turning right
+    // Begin turning right
+    rightTurnFast(); 
     delay(100);
-    //Update needed sensor values
-    lineMidRight = analogRead(lineMidRightPin);
+
+    // Update needed sensor values
+    updateLineSensors();
     printSensors();
-    //Test necessary conditions for completion of right turn
+
+    // Test if turn is complete
     if (!flag && lineMidRight < blackDetect) flag = true;
     if (flag && lineMidRight  > blackDetect) turnDone = true;
   }
 }
 
 void keepStraight(){
-  char turnFlag = 1;
-  while (turnFlag) {
+  bool flag = true;
+  while (flag) {
     walkForward();
     lineLeft = analogRead(lineLeftPin); lineRight = analogRead(lineRightPin); 
-    if (lineLeft < blackDetect && lineRight < blackDetect) turnFlag = 0;
+    if (lineLeft < blackDetect && lineRight < blackDetect) flag = false;
   }
 }
 
-void walkForward(){ //Moving forward full speed
+//------------------------------------------------------------------------------
+// Basic Movements
+//------------------------------------------------------------------------------
+
+//Moving forward full speed
+void walkForward(){ 
   leftWheel.write(137);  //137-92 = 45
   rightWheel.write(45);
 }
 
-void leftTurnSlow(){ //Slow turn: turn left with just one wheel
-  leftWheel.write(92); //Stop left wheel moving
-  rightWheel.write(0); //Move right wheel forward
+//Slow turn: turn left with just one wheel
+void leftTurnSlow(){ 
+  leftWheel.write(92); 
+  rightWheel.write(0); 
 }
 
-void rightTurnSlow(){ //Slow turn: turn right with one wheel
-  leftWheel.write(180); //Move left wheel forward
-  rightWheel.write(90); //Stop right wheel moving
+//Slow turn: turn right with one wheel
+void rightTurnSlow(){ 
+  leftWheel.write(180);
+  rightWheel.write(90); }
+
+//Fast turn: turn left with both wheels
+void leftTurnFast(){ 
+  leftWheel.write(0);   
+  rightWheel.write(0);  
 }
 
-void leftTurnFast(){ //Fast turn: turn left with both wheels
-  leftWheel.write(0);   //Move left wheel backward
-  rightWheel.write(0);  //Move right wheel forward
+//Fast turn: turn right with both wheels
+void rightTurnFast(){ 
+  leftWheel.write(180); 
+  rightWheel.write(180);
 }
 
-void rightTurnFast(){ //Fast turn: turn right with both wheels
-  leftWheel.write(180); //Move left wheel forward
-  rightWheel.write(180);//Move right wheel backward
-}
-
+//Drift: drift to the left 
 void leftDrift(){
   leftWheel.write(90);
   rightWheel.write(0);
 }
 
+//Drift: drift to the right
 void rightDrift(){
   leftWheel.write(180); //
   rightWheel.write(90); // 
+}
+
+//------------------------------------------------------------------------------
+// Other Functions
+//------------------------------------------------------------------------------
+
+void initializeStuff(){
+  // Setup Serial and pin directions
+  Serial.begin(9600);
+  pinMode(startPin, INPUT);
+
+  // Initialize system variables
+  leftWheel.attach(5);
+  rightWheel.attach(3);
+  lineMidLeftPin = A1;
+  lineMidRightPin = A2;
+  lineRightPin = A3;
+  lineLeftPin = A0;
+}
+
+void waitForStart(){
+  bool startFlag = false;
+  Serial.println("Waiting for button press");
+  while (!startFlag){
+    if (digitalRead(startPin) == HIGH) startFlag = true; 
+    leftWheel.write(92);  //Stop left wheel moving
+    rightWheel.write(90); //Stop right wheel forward
+  }
+  Serial.println("Starting...");
+}
+
+void updateLineSensors(){
+  lineMidLeft = analogRead(lineMidLeftPin); 
+  lineMidRight = analogRead(lineMidRightPin);
+  lineLeft = analogRead(lineLeftPin); 
+  lineRight = analogRead(lineRightPin);
+}
+
+void printSensors(){
+    // Used for debugging
+    // Prints line sensor values left to right
+  Serial.print(lineLeft);
+  Serial.print("  ");
+  Serial.print(lineMidLeft);
+  Serial.print("  ");
+  Serial.print(lineMidRight);
+  Serial.print("  ");
+  Serial.print(lineRight);
+  Serial.print("  ");
+  Serial.print(turn);
+  Serial.print("  ");
+  Serial.println();
 }
