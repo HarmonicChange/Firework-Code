9d0b2ed4def18ed3a37c20c9d21a3156b1c839c4
ikranz <ianmkranz@gmail.com>
2017-10-13 11:09:04 -0400

    Updated acoustics verilog (sin wave)

--- Optics/schmitt_read/schmitt_read.ino
+++ Optics/schmitt_read/schmitt_read.ino
@@ -1,9 +1,15 @@
 unsigned long lasttime = 0;
 unsigned long period = 0;
+int LED_7 = 3;
+int LED_12 = 4;
+int LED_17 = 5;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_7, OUTPUT);
+  pinMode(LED_12, OUTPUT);
+  pinMode(LED_17, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(2), treasure_ISR, FALLING);
 }
 
@@ -15,22 +21,23 @@ void treasure_ISR() {
 void loop() {
   //Serial.println(period);
   if(period > 130 && period < 150) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_7, HIGH);
     Serial.println("7kHz");  
     period = 0;
   }
   else if(period > 75 && period < 85) {
-    digitalWrite(LED_BUILTIN, HIGH);  
+    digitalWrite(LED_12, HIGH);  
     Serial.println("12kHz");  
     period = 0;
   }
   else if(period > 50 && period < 60) {
-    digitalWrite(LED_BUILTIN, HIGH); 
+    digitalWrite(LED_17, HIGH); 
     Serial.println("17kHz");   
     period = 0;
   }
   else {
-    digitalWrite(LED_BUILTIN, LOW);   
+    digitalWrite(LED_7, LOW);
+    digitalWrite(LED_12, LOW);
+    digitalWrite(LED_17, LOW);   
   }
-  delay(500); 
 }
