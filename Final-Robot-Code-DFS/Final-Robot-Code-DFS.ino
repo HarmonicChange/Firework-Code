d87a805db173720210b5263dfbd791e845fa92a9
cchanged <christina.changed@gmail.com>
2017-11-09 22:30:05 -0500

    functional milestone 3 code (the one in the video)

--- Final-Robot-Code-DFS/Final-Robot-Code-DFS.ino
+++ Final-Robot-Code-DFS/Final-Robot-Code-DFS.ino
@@ -11,7 +11,7 @@ int blackDetect = 600;      // Threshold above which sensors are reading a black
 int distanceInput; //wall sensor input pin, controlled by mux
 int IRInput; //treasure detector input pin, controlled by mux 
 int mux_S0 = 7, mux_S1 = 8; //00 = left, 10 = front, 01 = right
-
+int intRdy = 1;
 //-----DFS related control variables----Rearrange if desired//
 
 int currPos; //The coordinate number of the current intersection the robot is on
@@ -55,23 +55,28 @@ void setup() {
   lineLeftPin = A0;
 
   //Setup pins for wall/distance sensors.
-  //distanceInput = 4;
+  distanceInput = A4;
   //IRInput = ?;
   
   //Starting position and direction
   currPos = 19;
   currDir = north;
+
+
   
   //Initialize all nodes
   for(int i = 0; i < 20; i++) {
     grid[i] = new Node(i);
   }
 
+    //Initialize explorer
+  explorerPtr = new Explorer(grid[19]);
+
   /*Wait until starting signal - tone or button*/
   while(digitalRead(startPin) == LOW){
     leftWheel.write(92);
     rightWheel.write(90);
-    Serial.println("Waiting");
+    //Serial.println("Waiting");
     } //Wait while startPin is still low
 }
 
@@ -92,9 +97,16 @@ void loop() {
     //Serial.println("Right drifting");
     rightDrift();}
 
+  //Not at intersection
+  if (lineLeft < blackDetect && lineRight < blackDetect) intRdy = 1;
+  
   // At an intersection
-  if (lineLeft > blackDetect && lineRight > blackDetect) {
-    Serial.println("Intersection detected");
+  if (lineLeft > blackDetect && lineRight > blackDetect && intRdy) {
+    intRdy = 0;
+    Serial.print("Intersection detected:  ");
+    Serial.println(int(currPos));
+    Serial.print("Direction:  ");
+    Serial.println(int(currDir));
     /*********************Detect walls and treasures*/
     //Checks left sensor, add neighbor if no wall and unexplored
     if (isThereAWall(0)==1){ //Left wall present
@@ -105,7 +117,7 @@ void loop() {
       grid[currPos]->addNeighbor(grid[grid[currPos]->neighborCoord(currDir, 0, currPos)]); //Calls neighborCoord first to determine coord of adjacent node,
                                                                                            //then add to neighbor array
     }
-    
+
     //Checks right sensor, add neighbor if no wall and unexplored
     if (isThereAWall(2)==1){ //Right wall present
       grid[currPos]->addWall(Direction((currDir+1)%4), true);
@@ -127,20 +139,35 @@ void loop() {
     /*********************Turns toward next node, update DFS structure*/
     nextNode = explorerPtr->nextNode();
     nextPos  = nextNode->getCoord();
+    if (currPos == 2) {
+      leftWheel.write(92);
+      rightWheel.write(90);
+      Serial.println("is done!");
+      while(1);
+    }
+    Serial.print("nextPos:");
+    Serial.println(int(nextPos));
     //nextDir = if currDir is north and u turn left then = west, right = east, backward = south
     if(getTurn() == 1) {
       //turn in specified direction
+      Serial.println("Left turn");
       leftTurn(); 
     } else if (getTurn() == 2) {
-      leftTurn();
-      leftTurn();
+      Serial.println("U-turn");
+      leftWheel.write(92);
+      rightWheel.write(90);
+      Serial.println("is done!");
+      //leftTurnFast();
+      //leftTurnFast();
+      
     } else if (getTurn() == 3) {
+      Serial.println("right turn");
       rightTurn();
     }
   
     explorerPtr->travelTo(grid[nextPos]);
     currPos = nextPos;
-    currDir = nextDir;
+    //currDir = nextDir;
     //Robot should be following the line toward this next node now
   }    
 }
@@ -168,17 +195,17 @@ void printSensors(){
     lineMidLeft = analogRead(lineMidLeftPin);
     lineMidRight = analogRead(lineMidRightPin);
     lineRight = analogRead(lineRightPin);
-  Serial.print(lineLeft);
-  Serial.print("  ");
-  Serial.print(lineMidLeft);
-  Serial.print("  ");
-  Serial.print(lineMidRight);
-  Serial.print("  ");
-  Serial.print(lineRight);
-  Serial.print("  ");
+  //Serial.print(lineLeft);
+  //Serial.print("  ");
+  //Serial.print(lineMidLeft);
+  //Serial.print("  ");
+  //Serial.print(lineMidRight);
+ // Serial.print("  ");
+  //Serial.print(lineRight);
+  //Serial.print("  ");
 //  Serial.print(turn);
 //  Serial.print("  ");
-  Serial.println();
+  //Serial.println();
 }
 
 //Return if there is a wall 
@@ -190,12 +217,14 @@ int isThereAWall (int sensor){
   else if (sensor == 1){ //Y1 - front wall
       digitalWrite(mux_S0, HIGH);
       digitalWrite(mux_S1, LOW);
+      return (analogRead(distanceInput) > 130);
   }
   else if (sensor == 2){ //Y2 - right wall
       digitalWrite(mux_S0, LOW);
       digitalWrite(mux_S1, HIGH);
   }
-  
+  //Serial.println("distance");
+  //Serial.println(analogRead(distanceInput));
   return (analogRead(distanceInput) > 180);
 }
 
@@ -216,6 +245,7 @@ uint8_t wall (){
 // Complete 90 degree left turn at intersection
 void leftTurn(){
   bool turnDone = false;
+  currDir = (currDir-1)%4;
   //walkForward();
   //delay(200);
   bool flag = false;
@@ -228,6 +258,7 @@ void leftTurn(){
     //Test necessary conditions for completion of left turn
     if (!flag && lineMidLeft < blackDetect) flag = true;
     if (flag && lineMidLeft  > blackDetect) turnDone = true;
+    
   }
 }
 
@@ -237,15 +268,17 @@ void rightTurn(){
   //walkForward();
   //delay(200);
   bool flag = false;
+  currDir = (currDir+1)%4;
   while (! turnDone) {
     rightTurnFast(); ///Begin turning right
     delay(100);
     //Update needed sensor values
     lineMidRight = analogRead(lineMidRightPin);
-//    printSensors();
+    printSensors();
     //Test necessary conditions for completion of right turn
     if (!flag && lineMidRight < blackDetect) flag = true;
     if (flag && lineMidRight  > blackDetect) turnDone = true;
+    
   }
 }
 
@@ -259,8 +292,8 @@ void keepStraight(){
 }
 
 void walkForward(){ //Moving forward full speed
-  leftWheel.write(97);  //137-92 = 45
-  rightWheel.write(85);
+  leftWheel.write(94);  //137-92 = 45
+  rightWheel.write(88);
 }
 
 void leftTurnSlow(){ //Slow turn: turn left with just one wheel
@@ -276,11 +309,13 @@ void rightTurnSlow(){ //Slow turn: turn right with one wheel
 void leftTurnFast(){ //Fast turn: turn left with both wheels
   leftWheel.write(0);   //Move left wheel backward
   rightWheel.write(0);  //Move right wheel forward
+  //currDir = (currDir-1)%4;
 }
 
 void rightTurnFast(){ //Fast turn: turn right with both wheels
   leftWheel.write(180); //Move left wheel forward
   rightWheel.write(180);//Move right wheel backward
+  //currDir = (currDir+1)%4;
 }
 
 void leftDrift(){
